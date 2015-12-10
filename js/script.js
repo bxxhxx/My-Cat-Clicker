b79d5912a74ab39cfef4925b59177f205bc288f3
bxxhxx <Tom@New-Box-17-Edit.local>
2015-12-09 16:27:14 -0800

    VMO version refined
    After reviewing the solution, separated the views (list & picture), as
    well as separating init and render for each part. Also did minor
    changes accordingly to other JS part.

--- js/script.js
+++ js/script.js
@@ -1,98 +1,113 @@
-$(function() {
-            var model = {
+var model = {
 
-                init: function() {
+    init: function() {
 
-                    var Cat = function(name, imageURL) {
-                        var obj = Object.create(Cat.prototype);
-                        obj.name = name;
-                        obj.imageURL = imageURL;
-                        obj.score = 0;
-                        return obj;
-                    };
-
-                    Cat.prototype.clicker = function() {
-                        this.score += 1;
-                    }
-
-                    var catNames = ['Shelly', 'Kelly', 'Bob', 'Joe', 'Sweet'];
-
-                    for (i = 0; i < 5; i++) {
-                        var catImageURL = 'images/cat' + (i + 1) + '.jpg';
-                        var nextCat = Cat(catNames[i], catImageURL);
-                        this.bigList.push(nextCat);
-                    }
-                },
-
-                currentCat: {},
-                bigList: []
-            };
-
-
-            var octopus = {
-
-                init: function() {
-                    model.init();
-                    view.init();
-                },
-
-                catTicker: function(i) {
-                    return model.bigList[i].name;
-
-                },
-
-                score: function() {
-                    model.currentCat.score += 1;
-                    return model.currentCat.score;
-
-                },
-
-            load: function(arrayPosition) {
-                model.currentCat = model.bigList[arrayPosition];
-                view.load(model.currentCat);
-
-            }
+        var Cat = function(name, imageURL) {
+            var obj = Object.create(Cat.prototype);
+            obj.name = name;
+            obj.imageURL = imageURL;
+            obj.score = 0;
+            return obj;
         };
 
+        Cat.prototype.clicker = function() {
+            this.score += 1;
+        }
 
-        var view = {
-            nameHTML: document.getElementById('name'),
-            scoreHTML: document.getElementById('score'),
-            imageURLHTML: document.getElementById('imageURL'),
+        var catNames = ['Shelly', 'Kelly', 'Bob', 'Joe', 'Sweet'];
 
-            init: function() {
-                var listHTML = document.getElementById('list');
+        for (i = 0; i < 5; i++) {
+            var catImageURL = 'images/cat' + (i + 1) + '.jpg';
+            var nextCat = Cat(catNames[i], catImageURL);
+            this.bigList.push(nextCat);
+        }
+    },
 
-                for (i = 0; i < 5; i++) {
-                    var catListItem = document.createElement('li');
-                    catListItem.textContent = octopus.catTicker(i);
-                    listHTML.appendChild(catListItem);
-                    var currentListItem = listHTML.lastChild;
-                    currentListItem.addEventListener('click', (function(icopy) {
-                        return function() {
-                            octopus.load(icopy);
-                        };
-                    })(i));
+    currentCat: {},
+    bigList: []
+};
+
+
+var octopus = {
+
+    init: function() {
+        model.init();
+        //set our current cat to the first one in the list
+        model.currentCat = model.bigList[0];
+        viewP1.init();
+        viewP2.init();
+    },
+
+    catTicker: function(i) {
+        return model.bigList[i];
+
+    },
+
+    score: function() {
+        model.currentCat.score += 1;
+        return model.currentCat.score;
+
+    },
+
+    load: function(catObject) {
+        model.currentCat = catObject;
+        viewP2.render(model.currentCat);
+
+    },
+
+    currentCat: function() {
+        return model.currentCat;
+    }
+};
+
+
+var viewP1 = {
+
+    init: function() {
+        this.listHTML = document.getElementById('list');
+        this.render();
+    },
+
+    render: function() {
+        for (i = 0; i < 5; i++) {
+            var catListItem = document.createElement('li');
+            var cat = octopus.catTicker(i);
+            catListItem.textContent = cat.name;
+            this.listHTML.appendChild(catListItem);
+            var currentListItem = this.listHTML.lastChild;
+            currentListItem.addEventListener('click', (function(catCopy) {
+                return function() {
+                    octopus.load(catCopy);
                 }
+            })(cat));
+        }
+    }
+};
 
-                this.imageURLHTML.addEventListener('click', function(e) {
-                    var newScore = octopus.score();
-                    //console.log(this);
-                    view.scoreHTML.textContent = 'Number of Clicks: ' + newScore;
-                });
-            },
 
-            load: function(currentCat) {
-                //MIGRTE TO VIEW
-                this.nameHTML.textContent = currentCat.name;
+var viewP2 = {
 
-                this.scoreHTML.textContent = 'Number of Clicks: ' + currentCat.score;
+    init: function() {
+        this.nameHTML = document.getElementById('name');
+        this.scoreHTML = document.getElementById('score');
+        this.imageURLHTML = document.getElementById('imageURL');
 
-                this.imageURLHTML.src = currentCat.imageURL;
+        this.imageURLHTML.addEventListener('click', function(e) {
+            var newScore = octopus.score();
+            //console.log(this);
+            viewP2.scoreHTML.textContent = 'Number of Clicks: ' + newScore;
+        });
 
-            }
+        this.render(octopus.currentCat());
+    },
 
-        };
+    render: function(currentCat) {
+        this.nameHTML.textContent = currentCat.name;
+        this.scoreHTML.textContent = 'Number of Clicks: ' + currentCat.score;
+        this.imageURLHTML.src = currentCat.imageURL;
 
-        octopus.init();
-    });
+    }
+};
+
+
+octopus.init();
